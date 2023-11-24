e7aca126d7f392ca621a84faee26b9fa4ce25eae
1038873 <lokesh.guguloth@quest-global.com>
2023-11-24 10:14:55 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-
 const app = express()
 const cors = require('cors')
 
@@ -13,16 +12,29 @@ const updateStudent = require('./routes/student/updateUser')
 
 const addNewUser = require('./routes/user/addUserRoute')
 const getUser = require('./routes/user/getUser')
+const productListFromAdmin = require('./routes/user/getProducts')
+const addToCart = require('./routes/user/addToCart')
+const cart = require('./routes/user/addToCart')
+
 
 
 
 app.use(bodyParser.json())
 app.use(cors())
 
+//adminRoutes
+// app.use('/api', admin)
 
+
+//userRoutes
 app.use('/api', getUser)
 app.use('/api', addNewUser)
+app.use('/api', productListFromAdmin)
 
+app.use('/api', cart)
+
+
+//StudentRoutes
 app.use('/api', getStudents)
 app.use('/api', userAdd)
 app.use('/api', deleteUserRoute)
